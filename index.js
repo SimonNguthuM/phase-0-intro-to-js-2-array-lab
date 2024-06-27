2fd60986737fb11d9e1f482ab0d51577ebdd409f
Simon Nguthu <simon.nguthu@student.moringaschool.com>
2024-06-27 12:44:14 +0300

    arrays

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name){
+    return cats.push('Ralph');
+}
+function destructivelyPrependCat(name){
+    return cats.unshift('Bob');
+}
+function destructivelyRemoveLastCat(){
+    return cats.pop();
+}
+function destructivelyRemoveFirstCat(){
+    return cats.shift();
+}
+function appendCat(cats2){
+    return cats2 = [...cats, "Broom"]
+}
+function prependCat(cats2){
+    return cats2 = ["Arnold", ...cats,]
+}
+function removeLastCat(){
+    return cats.slice (0, 2);
+}
+function removeFirstCat(){
+    return cats.slice (-2);
+}
